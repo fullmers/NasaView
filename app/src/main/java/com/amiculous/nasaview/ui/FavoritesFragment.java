f0a90687bcfa91dd56533e69409e2f0110a82946
Sarah Fullmer <sarah.fullmer@gmail.com>
2019-02-22 14:07:37 +0100

    feat: define adapter and attach to recycler view

--- app/src/main/java/com/amiculous/nasaview/ui/FavoritesFragment.java
+++ app/src/main/java/com/amiculous/nasaview/ui/FavoritesFragment.java
@@ -5,15 +5,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.amiculous.nasaview.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FavoritesFragment extends Fragment {
 
-    private FavoritesViewModel mViewModel;
+    private FavoritesViewModel favoritesViewModel;
+    @BindView(R.id.favorites) RecyclerView recyclerView;
 
     public static FavoritesFragment newInstance() {
         return new FavoritesFragment();
@@ -23,13 +30,20 @@ public class FavoritesFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.favorites_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.favorites_fragment, container, false);
+        ButterKnife.bind(this, rootView);
+
+        final FavoritesListAdapter adapter = new FavoritesListAdapter(getActivity());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return rootView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(FavoritesViewModel.class);
+        favoritesViewModel = ViewModelProviders.of(this).get(FavoritesViewModel.class);
         // TODO: Use the ViewModel
     }
 
---
+++ app/src/main/java/com/amiculous/nasaview/ui/FavoritesListAdapter.java
@@ -0,0 +1,77 @@
+package com.amiculous.nasaview.ui;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.amiculous.nasaview.R;
+import com.amiculous.nasaview.data.ApodEntity;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class FavoritesListAdapter extends RecyclerView.Adapter<FavoritesListAdapter.FavoritesViewHolder> {
+
+
+    private final LayoutInflater inflater;
+    private List<ApodEntity> favorites;
+    private Context context;
+
+    FavoritesListAdapter(Context context) {inflater = LayoutInflater.from(context);
+    this.context = context;}
+
+    @Override
+    public FavoritesViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View itemView = inflater.inflate(R.layout.favorites_item,parent,false);
+        return new FavoritesViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(FavoritesViewHolder holder, int position) {
+        if (favorites != null ) {
+            ApodEntity current = favorites.get(position);
+            holder.titleTextView.setText(current.getTitle());
+            holder.descPreviewTextView.setText(current.getExplanation());
+
+            Picasso.with(context.getApplicationContext())
+                    .load(current.getUrl())
+                    .placeholder(context.getResources().getDrawable(R.drawable.default_apod))
+                    .into(holder.imagePreviewImageView);
+        } else {
+            holder.titleTextView.setText("No favorites saved yet");
+        }
+    }
+
+    void setFavorites(List<ApodEntity> favorites) {
+        this.favorites = favorites;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (favorites != null) {
+            return favorites.size();
+        } else return 0;
+    }
+
+
+
+    class FavoritesViewHolder extends RecyclerView.ViewHolder {
+        private final ImageView imagePreviewImageView;
+        private final TextView titleTextView;
+        private final TextView descPreviewTextView;
+
+        private FavoritesViewHolder(View itemView) {
+            super(itemView);
+            imagePreviewImageView = itemView.findViewById(R.id.preview_image);
+            titleTextView = itemView.findViewById(R.id.title_text);
+            descPreviewTextView = itemView.findViewById(R.id.desc_text);
+
+        }
+    }
+
+}
