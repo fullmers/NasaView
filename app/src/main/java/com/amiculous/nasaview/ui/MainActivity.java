09c659566acefe8b2d2bcb64513924cc0b6b9340
Sarah Fullmer <sarah.fullmer@gmail.com>
2019-04-06 16:58:28 +0200

    fix: bottom nav bar navigation

--- app/src/main/java/com/amiculous/nasaview/ui/MainActivity.java
+++ app/src/main/java/com/amiculous/nasaview/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -14,51 +15,64 @@ import com.amiculous.nasaview.ui.apod.ApodFragment;
 import com.amiculous.nasaview.ui.favorites.FavoritesFragment;
 import com.amiculous.nasaview.ui.search.SearchFragment;
 
-public class MainActivity extends AppCompatActivity {
+import timber.log.Timber;
 
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+
+    android.support.design.widget.BottomNavigationView navigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        android.support.design.widget.BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        //TODO this behavior is buggy. seems on in 27, but not 28. Investigate and fix
-/*      android.support.design.widget.CoordinatorLayout.LayoutParams layoutParams = (android.support.design.widget.CoordinatorLayout.LayoutParams) navigation.getLayoutParams();
-        layoutParams.setBehavior(new BottomNavigationBarBehavior());
-*/
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setSelectedItemId(R.id.navigation_apod);
-        loadFragment(ApodFragment.newInstance());
+        commitFragment(ApodFragment.newInstance(), false);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment favorites = FavoritesFragment.newInstance();
-            switch (item.getItemId()) {
-                case R.id.navigation_favorites:
-                    loadFragment(favorites);
-                    return true;
-                case R.id.navigation_apod:
-                    Fragment apod = ApodFragment.newInstance();
-                    loadFragment(apod);
-                    return true;
-                case R.id.navigation_search:
-                    Fragment search = new SearchFragment();
-                    loadFragment(search);
-                    return true;
-            }
-            return false;
+    /**
+     * Take user back to Apod tab if not there, otherwise backpress normally
+     */
+    @Override
+    public void onBackPressed() {
+        int selectedItemId = navigation.getSelectedItemId();
+        if (R.id.navigation_apod != selectedItemId) {
+            navigation.setSelectedItemId(R.id.navigation_apod);
+        } else {
+            super.onBackPressed();
         }
-    };
+    }
 
-    private void loadFragment(Fragment fragment) {
-        //TODO this back navigation is funny. figure out why and fix it.
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        getSupportFragmentManager().popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        switch (item.getItemId()) {
+            case R.id.navigation_favorites:
+                commitFragment(FavoritesFragment.newInstance(), true);
+                break;
+            case R.id.navigation_apod:
+                commitFragment(ApodFragment.newInstance(), true);
+                break;
+            case R.id.navigation_search:
+                commitFragment(new SearchFragment(), true);
+                break;
+        }
+        return true;
+    }
+
+    private void commitFragment(@NonNull Fragment fragment, boolean keep) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_container, fragment);
-        transaction.addToBackStack(null);
+        if (keep) {
+            Fragment oldFragment = getSupportFragmentManager().findFragmentById(R.id.frame_container);
+            if (oldFragment != null) {
+                transaction.hide(oldFragment);
+            }
+            transaction.add(R.id.frame_container, fragment);
+            transaction.addToBackStack(fragment.getClass().getName());
+        } else {
+            transaction.replace(R.id.frame_container, fragment);
+        }
         transaction.commit();
     }
 
