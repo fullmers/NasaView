7e48c640a2ea211120f15f3204debea711fa5dee
Sarah Fullmer <sarah.fullmer@gmail.com>
2019-04-21 15:09:05 +0200

    feat: implement simple widget layout

--- app/src/main/java/com/amiculous/nasaview/ApodWidget.java
+++ app/src/main/java/com/amiculous/nasaview/ApodWidget.java
@@ -13,13 +13,14 @@ public class ApodWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+     /*   CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.apod_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+        */
     }
 
     @Override
